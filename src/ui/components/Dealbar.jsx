3ba419ab9e13daba2e8841e38a7b032c3403d36b
FJBeltranPujante <kvortvaj@gmail.com>
2022-09-07 17:42:31 +0200

    original

--- src/ui/components/Dealbar.jsx
+++
@@ -1,9 +0,0 @@
-import './Dealbar.css';
-
-export const Dealbar = () => {
-  return (
-    <div className='deal__container'>
-      <p className="deal">FREE Delivery and Return up until October!</p>
-      </div>
-  )
-}
